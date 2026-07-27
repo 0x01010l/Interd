b8383c9669e75fde660c7ad4c4dfdef389ab0b20
0x01010l <102048580+0x01010l@users.noreply.github.com>
2026-07-27 17:32:43 +0100

    Update Contact form with Tally embed and script loader

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,21 +4,8 @@ import { useEffect } from 'react';
 
 export default function Contact() {
   useEffect(() => {
-    // Load Tally script
     const script = document.createElement('script');
-    script.innerHTML = `
-      var d=document,w="https://tally.so/widgets/embed.js",v=function(){
-        "undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))
-      };
-      if("undefined"!=typeof Tally)v();
-      else if(d.querySelector('script[src="'+w+'"]')==null){
-        var s=d.createElement("script");
-        s.src=w;
-        s.onload=v;
-        s.onerror=v;
-        d.body.appendChild(s);
-      }
-    `;
+    script.textContent = `var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}`;
     document.body.appendChild(script);
   }, []);
 
